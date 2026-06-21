9a53c7ed227625c236fa2204ccd68d348aec31cf
sampaceinstitute <samuelanuoluwapoayeni@gmail.com>
2026-06-21 11:56:06 +0100

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
+  },
+  server: {
+    port: 3000,
+  }
+})
